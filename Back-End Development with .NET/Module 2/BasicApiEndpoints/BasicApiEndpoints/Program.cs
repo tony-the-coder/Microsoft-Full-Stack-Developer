ae7d6bbdf74f34de37b46e7fe348aa5bd50e2a87
Tony Aldea <tonythecoder@outlook.com>
2025-07-07 17:14:26 -0400

    Add ProductsController and HTTP request examples for product management

--- Back-End Development with .NET/Module 2/BasicApiEndpoints/BasicApiEndpoints/Program.cs
+++ Back-End Development with .NET/Module 2/BasicApiEndpoints/BasicApiEndpoints/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
+// Remove HTTPS redirection so you can test with http
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
