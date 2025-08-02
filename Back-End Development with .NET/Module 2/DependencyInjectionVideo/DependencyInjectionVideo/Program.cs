1354b553a19ac8d96e1658173720c1ef0f4fce78
Tony Aldea <tonythecoder@outlook.com>
2025-08-02 17:17:13 -0400

    Add initial project files for DependencyInjectionVideo solution and setup basic web application

---
+++ Back-End Development with .NET/Module 2/DependencyInjectionVideo/DependencyInjectionVideo/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+builder.Services.AddSingleton<IMyService, MyService>(); 
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+public interface IMyService
+{
+    void LogCreation(string message);
+}
+
+public class MyService : IMyService
+{
+    private readonly int _serviceId;
+
+    public MyService()
+    {
+        _serviceId = new Random().Next(100000, 999999);
+    }
+
+    public void LogCreation(string message)
+    {
+        Console.WriteLine($"Service {_serviceId} created: {message}");
+    }
+}
